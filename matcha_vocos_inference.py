f6bcd40c86a1e9db29f67c04d8b557e98aa409a8
Jordi Mas <jmas@softcatala.org>
2024-10-16 22:16:05 +0200

    threads

--- matcha_vocos_inference.py
+++ matcha_vocos_inference.py
@@ -148,7 +148,10 @@ if __name__ == "__main__":
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")    
     threads = torch.get_num_threads()
-    print(f"Matcha threads:  {threads}")
+    print(f	"Matcha threads:  {threads}")
+    threads = torch.get_num_threads()
+    torch.set_num_threads(8)
+    print(f	"Matcha threads:  {threads}")
 
     # load Matxa from HF
     model = load_model_from_hf(matxa, device=device).to(device)
